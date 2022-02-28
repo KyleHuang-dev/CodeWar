a21f2b01e483caa9ac52ec13cae0cd3a1377baaa
your_github_username <kyle.huang.dev@gmail.com>
2022-02-28 04:47:35 -0700

    Find the max and min number. Second solution, solve by sort method

--- Find Maximum and Minimum Values of a List.js
+++ Find Maximum and Minimum Values of a List.js
@@ -16,4 +16,12 @@ var max = function(list){
 
 
 
-
+// by sort
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+var max = function(list){
+    list.sort((a, b) => (b - a));
+    return list[0];
+}
