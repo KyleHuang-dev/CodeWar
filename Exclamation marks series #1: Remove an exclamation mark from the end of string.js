0b1d85ce424a246e75172dc611ff633430a94331
your_github_username <kyle.huang.dev@gmail.com>
2022-03-11 09:35:23 -0700

    Replace the exclamation marks in the end the sentence, by using endsWith method

--- Exclamation marks series #1: Remove an exclamation mark from the end of string.js
+++ Exclamation marks series #1: Remove an exclamation mark from the end of string.js
@@ -13,3 +13,7 @@
 function remove (string) {
     return string[string.length -1] ==="!" ? string.slice(0,-1): string
   }
+
+  function remove(s) {
+    return s.endsWith('!') ? s.slice(0, -1) : s;
+  }
