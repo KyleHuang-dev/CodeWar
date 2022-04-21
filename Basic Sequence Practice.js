750763b0f5d09bf58dfdad64a8f223fbd339cd2a
your_github_username <kyle.huang.dev@gmail.com>
2022-04-21 10:14:19 -0600

    Created the array of sequence number, second solution

--- Basic Sequence Practice.js
+++ Basic Sequence Practice.js
@@ -22,3 +22,12 @@ function sumOfN(n) {
     }
     return arr
   };
+
+// other solution
+function sumOfN(n) {
+    for (var i = 0, list = []; i <= Math.abs(n); i++) {
+      list.push((list[i - 1] || 0) + i * (n < 0 ? -1 : 1))
+    }
+    
+    return list
+  }
