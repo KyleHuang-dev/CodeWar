b7b188467891db034c88ffe6592e7fbff4f5cf8c
your_github_username <kyle.huang.dev@gmail.com>
2022-04-25 02:04:54 -0600

    Push the array with 2 values alternating, second solution

--- Length and two values..js
+++ Length and two values..js
@@ -17,4 +17,13 @@ function alternate(n, firstValue, secondValue){
     return arr.slice(0,n)
   }
 
-  
+
+// other solution
+
+function alternate(n, firstValue, secondValue){
+    const array = [];
+    for (let i = 0; i < n; i++) {
+      array.push(i % 2 === 0 ? firstValue : secondValue)
+    }
+    return array;
+  }
