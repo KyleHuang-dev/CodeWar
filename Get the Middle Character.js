80d5a67764e2e78c7ff8e702d3c82aafb89ddf60
your_github_username <kyle.huang.dev@gmail.com>
2022-05-04 10:58:08 -0600

    Find the middle letter in the text

--- Get the Middle Character.js
+++ Get the Middle Character.js
@@ -20,5 +20,7 @@
 
 function getMiddle(s)
 {
-  return s.length % 2 ==0 ? s.slice(s.length/2-1, s.length/2 +1): s.charAt(Math.floor(s.length/2))
+  return s.length % 2 == 0 
+    ? s.slice(s.length/2-1, s.length/2 +1)
+    : s.charAt(Math.floor(s.length/2))
 }
