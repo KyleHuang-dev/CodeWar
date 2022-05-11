137a55b853af7699542843b1bbfaa783865ad99e
your_github_username <kyle.huang.dev@gmail.com>
2022-05-11 08:57:25 -0600

    Sort the array

---
+++ Two to One.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  
+    //concat the two string to array
+    //sort the array
+    //push the el if its not exist
+    const arr = [...s1].concat([...s2])
+    
+    return arr.sort().reduce( (acc,cur)=> {
+      if(acc.indexOf(cur)===-1){
+        acc.push(cur)
+      }return acc
+    },[]).join('')
+  }
