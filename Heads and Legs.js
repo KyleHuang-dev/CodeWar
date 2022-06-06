b30453cd8b620f1790603b3678bccac928ed84cf
your_github_username <kyle.huang.dev@gmail.com>
2022-06-06 09:53:15 -0600

    Heads and Legs

--- Heads and Legs.js
+++ Heads and Legs.js
@@ -1,30 +1,39 @@
-#Description
+// #Description
 
-Everybody has probably heard of the animal heads and legs problem from the earlier years at school. It goes:
+// Everybody has probably heard of the animal heads and legs problem from the earlier years at school. It goes:
 
-“A farm contains chickens and cows. There are x heads and y legs. How many chickens and cows are there?”
+// “A farm contains chickens and cows. There are x heads and y legs. How many chickens and cows are there?”
 
-Where x <= 1000 and y <=1000
+// Where x <= 1000 and y <=1000
 
-#Task
+// #Task
 
-Assuming there are no other types of animals, work out how many of each animal are there.
+// Assuming there are no other types of animals, work out how many of each animal are there.
 
-Return a tuple in Python - (chickens, cows) and an array list - [chickens, cows]/{chickens, cows} in all other languages
+// Return a tuple in Python - (chickens, cows) and an array list - [chickens, cows]/{chickens, cows} in all other languages
 
-If either the heads & legs is negative, the result of your calculation is negative or the calculation is a float return "No solutions" (no valid cases), or [-1, -1] in COBOL.
+// If either the heads & legs is negative, the result of your calculation is negative or the calculation is a float return "No solutions" (no valid cases), or [-1, -1] in COBOL.
 
-In the form:
+// In the form:
 
-[Heads, Legs] = [72, 200]
+// [Heads, Legs] = [72, 200]
 
-VALID - [72, 200] =>             [44 , 28]   
-                             [Chickens, Cows]
+// VALID - [72, 200] =>             [44 , 28]   
+//                              [Chickens, Cows]
 
-INVALID - [72, 201] => "No solutions"
-However, if 0 heads and 0 legs are given always return [0, 0] since zero heads must give zero animals.
+// INVALID - [72, 201] => "No solutions"
+// However, if 0 heads and 0 legs are given always return [0, 0] since zero heads must give zero animals.
 
-There are many different ways to solve this, but they all give the same answer.
+// There are many different ways to solve this, but they all give the same answer.
 
-You will only be given integers types - however negative values (edge cases) will be given.
+// You will only be given integers types - however negative values (edge cases) will be given.
 
+function animals(heads, legs){
+    const chickens = (4 * heads - legs) / 2;
+    const cows = heads - chickens;
+    
+    if (chickens >= 0 && chickens % 1 === 0 && cows >= 0) {
+        return [chickens, cows];
+    }
+    return 'No solutions';
+  }
