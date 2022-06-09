04869682824822b868f96fc89bf14abd30fd77c1
your_github_username <kyle.huang.dev@gmail.com>
2022-06-09 10:05:39 -0600

    Check boolean of array of true and false

---
+++ Logical calculator.js
@@ -0,0 +1,20 @@
+// Your Task
+// Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+// Examples
+// booleans = [True, True, False], operator = "AND"
+// True AND True -> True
+// True AND False -> False
+// return False
+// booleans = [True, True, False], operator = "OR"
+// True OR True -> True
+// True OR False -> True
+
+const logicalCalc = (array, op) => {
+    return array.reduce((acc, cur) => {
+      if (op === 'AND') return acc && cur;
+      if (op === 'OR') return acc || cur;
+      if (op === 'XOR') return acc != cur;
+    });
+  };
+
