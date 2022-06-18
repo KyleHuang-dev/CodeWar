90d9ffdc66e4d3fd1994dcda6ae6e8a1b0275d04
your_github_username <kyle.huang.dev@gmail.com>
2022-06-18 01:46:11 -0600

    Filter the array, only return the numbers, additional solution

--- List Filtering.js
+++ List Filtering.js
@@ -9,3 +9,8 @@ function filter_list(l) {
     let arr = l.filter(n=> Number.isInteger(n))
     return arr
   }
+
+// other solution with typeof
+function filter_list(l) {
+    return l.filter(n=> typeof(n)=='number')
+  }
