0d0667accb354b22a4322424b9f4b7b011d34dfa
your_github_username <kyle.huang.dev@gmail.com>
2022-06-22 14:10:19 -0600

    Check if the word had same number of x and o.

---
+++ Exes and Ohs.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    let arrStr= [...str.toLowerCase()]
+    for(i=0,x=0,o=0; i<arrStr.length; i++){
+      arrStr[i] === 'x'? x++ : arrStr[i] === 'o'? o++ : null
+    }
+    return x === o? true: false
+  }
