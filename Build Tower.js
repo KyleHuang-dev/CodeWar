5b12fbdee576635bd75eda4c11f2f96cd1d8f5a4
your_github_username <kyle.huang.dev@gmail.com>
2022-07-14 08:29:46 -0600

    Build the tower with the stars.

---
+++ Build Tower.js
@@ -0,0 +1,32 @@
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+
+function towerBuilder(nFloors) {
+    let tower = [];
+    let space;
+    let star;
+    for ( i = 0 ; i < nFloors; i++){
+      space = ' '.repeat(nFloors - 1 - i)
+      star = '*'.repeat(i*2 +1)
+      tower[i]= space + star + space
+    } 
+    return tower
+  }
