f4e260017ece4e99801b04a87537c31ee37e33c4
your_github_username <kyle.huang.dev@gmail.com>
2022-07-26 08:27:57 -0600

    Convert the title to proper case, add other solution

--- title-case.js
+++ title-case.js
@@ -28,3 +28,16 @@ function titleCase(title, minorWords="") {
   function properCase(str){
       return str[0].toUpperCase() + str.slice(1).toLowerCase()
   }
+
+  // other solution
+  function titleCase(title, minorWords) {
+    var minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+    return title.toLowerCase().split(' ').map(function(v, i) {
+      if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+        v = v.split('');
+        v[0] = v[0].toUpperCase();
+        v = v.join('');
+      }
+      return v;
+    }).join(' ');
+  }
