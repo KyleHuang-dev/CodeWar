54550f01cb152922813ac911aa418c30133f532c
your_github_username <kyle.huang.dev@gmail.com>
2022-07-29 13:09:45 -0600

    Replace the string with other string basic on if the string apprear more than once in the array

---
+++ duplicate-encoder.js
@@ -0,0 +1,19 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+function duplicateEncode(word){
+    let newArr = word.toLowerCase().split('').map((letter,i,arr) =>{
+       if (arr.filter(el=> el === letter).length > 1)
+         letter =')'
+       else 
+         letter = '('
+      return letter
+     })
+    return newArr.join('')
+   }
+   
