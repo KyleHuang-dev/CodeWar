c738294d1dca12183606bffad56f9e6f36f5af9d
your_github_username <kyle.huang.dev@gmail.com>
2022-08-08 10:13:00 -0600

    Find the remainder of the number divided 13, using reduce method

--- a-rule-of-divisibility by-13.js
+++ a-rule-of-divisibility by-13.js
@@ -61,3 +61,11 @@ function thirt(n) {
   
     return sum
 }
+
+// reduce method
+function thirt(n) {
+    const nums = [1,10,9,12,3,4]
+    var sum = (''+n).split('').reverse().reduce((sum,v,i) => sum + v * nums[i%nums.length], 0)
+    return sum === n ? n : thirt(sum)
+  }
+  
