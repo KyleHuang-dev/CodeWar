2ee06691838615faa7fbb3e36c394bfc1d88b158
your_github_username <kyle.huang.dev@gmail.com>
2022-08-23 02:44:39 -0600

    Switch the letter a and b

---
+++ switcheroo.js
@@ -0,0 +1,22 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+
+
+//convert string to array
+// map through the array replace the a to b, b to a
+// return string
+
+function switcheroo(x){
+    return x.split('').map(letter => {
+      if(letter === 'a')
+        letter = 'b';
+      else if (letter === 'b')
+        letter = 'a'
+      return letter
+    }).join('')
+  }
