6d8e9fbc8bd551bb6d44e19147d92b5f2459b39c
your_github_username <kyle.huang.dev@gmail.com>
2022-09-09 09:37:43 -0600

    Moving the zeros to the end of the array, addtional solution with concat method

--- moving-zeros-to-the-end.js
+++ moving-zeros-to-the-end.js
@@ -10,3 +10,8 @@ function moveZeros(arr) {
     }
     return noZeroArr;
   }
+
+//other solution with concat
+function moveZeros(arr) {
+    return arr.filter(el => el !== 0).concat(arr.filter(el => el ===0))
+  }
