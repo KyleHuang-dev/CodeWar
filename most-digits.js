b4bbb4cc38fdec5e87d7e07f0e88fa046d47158e
your_github_username <kyle.huang.dev@gmail.com>
2022-09-12 11:47:47 -0600

    Find the number with the most digits, additional solution with reduce method

--- most-digits.js
+++ most-digits.js
@@ -9,3 +9,11 @@ function findLongest(array){
     let length = array.map(num => String(num).length);
     return array[length.indexOf(Math.max(...length))]
  }
+
+ //other solution with reduce method
+
+ function findLongest(array){
+    return array.reduce( (acc, cur) =>{
+      return String(acc).length < String(cur).length ? cur : acc;  
+    })
+  }
