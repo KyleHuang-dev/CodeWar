396c325caccf0ee437b1e09c621fc232edec4559
your_github_username <kyle.huang.dev@gmail.com>
2022-09-20 01:23:31 -0600

    Find the number of times that a number multiply itself, added other solution

---
+++ persistent-bugger.js
@@ -0,0 +1,35 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+function persistence(num) {
+    let i = 0
+    while (String(num).length >1){
+      num = multipleItself(num);
+      i++
+    }
+    return i
+  }
+  
+  function multipleItself (num){
+    let numArr = String(num).split('')
+    return numArr.reduce((acc,cur)=> acc * cur,1)
+  }
+
+// additional solution
+function persistence(num) {
+    var times = 0;
+    
+    num = num.toString();
+    
+    while (num.length > 1) {
+      times++;
+      num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+    
+    return times;
+ }
